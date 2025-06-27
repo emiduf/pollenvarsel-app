af7837960feffc955378e5a5db2963ff4d700c8e
emiduf <emiliedufseth@hotmail.com>
2025-06-27 17:38:36 +0200

    Css lagt til i knapp og input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [location, setLocation] = useState('')
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Brukerens lokasjon: ${location}')
+    //Her kan man senere hente pollendata basert på lokasjon 
+  }
+
   return (
     <div className="app">
       <h1>Pollenvarsel 🌿</h1>
       <p>Her vises dagens pollennivå basert på din lokasjon📍</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+        type="text"
+        placeholder="Skriv inn din by eller sted"
+        value={location}
+        onChange={handleInputChange}
+        />
+        <button type="submit">Sjekk pollennivå</button>
+      </form>
     </div>
   )
 }
